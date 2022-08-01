242d4b13668edeb8958dea156718770533b7628e
WoojinKim <dnwls488@naver.com>
2022-08-01 23:24:51 +0900

    프록시 내부 호출 문제 해결1 - 자기 자신 호출 (순환 참조 주의)

---
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,32 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    /**
+     * Spring Boot 2.6 릴리즈 노트 - 순환 참조가 기본적으로 금지 됨
+     * application properties
+     * spring.main.allow-circular-references=true
+     */
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}", callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal(); //외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
